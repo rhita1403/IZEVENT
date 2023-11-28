9fd0feee82340b522681e3396e185698f9521a1a
william varsovie <william.varsovie@gmail.com>
2023-11-28 13:18:28 +0100

    fini

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,46 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    @event.save
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path, status: :see_other
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :date, :description)
+  end
+end
