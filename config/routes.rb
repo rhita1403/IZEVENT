79e3a79863aecadf8c21aaa798f92d681ed2102c
Edward Clary <edwardclary@gmail.com>
2023-12-04 16:39:29 +0100

    Invite new user corrected

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
      get :add_multiple_tasks
      get :add_participation
      get :add_multiple_participations
+     get :add_multiple_new_participations
+     get :add_new_participation
     end
   end
 
