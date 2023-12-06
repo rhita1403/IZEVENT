aa41b1b8741524b36d728d80be2cb50b5e4962b0
Edward Clary <edwardclary@gmail.com>
2023-12-06 15:49:43 +0100

    Sign in and sign up pages fixed. Homepage changed and address auto fill done, needs front

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :phone_number, presence: true, uniqueness: true
 
   def to_label
     "#{first_name} #{last_name}"
